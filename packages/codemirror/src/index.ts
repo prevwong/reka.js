4a96530386dd90f07afa129a73ba97f7f03d1287
Prev Wong <prevwong@gmail.com>
2022-09-06 00:53:10 +0800

    chore: lint

--- packages/codemirror/src/index.ts
+++ packages/codemirror/src/index.ts
@@ -1,12 +1,12 @@
-import { styleTags, tags as t } from "@codemirror/highlight";
+import { styleTags, tags as t } from '@codemirror/highlight';
 import {
   LRLanguage,
   LanguageSupport,
   indentNodeProp,
   foldNodeProp,
-} from "@codemirror/language";
+} from '@codemirror/language';
 
-import { parser } from "./parser";
+import { parser } from './parser';
 
 export const compositeLanguage = LRLanguage.define({
   parser: parser.configure({
@@ -14,31 +14,31 @@ export const compositeLanguage = LRLanguage.define({
       indentNodeProp.add({}),
       foldNodeProp.add({}),
       styleTags({
-        "component val": t.keyword,
+        'component val': t.keyword,
         String: t.string,
         Number: t.number,
-        "ComponentDeclaration/VariableName": t.function(
+        'ComponentDeclaration/VariableName': t.function(
           t.definition(t.variableName)
         ),
         VariableName: t.variableName,
-        "VariableDeclaration/VariableName": t.definition(t.variableName),
-        "(": t.paren,
-        ")": t.paren,
-        "{": t.brace,
-        "}": t.brace,
-        "=>": t.keyword,
+        'VariableDeclaration/VariableName': t.definition(t.variableName),
+        '(': t.paren,
+        ')': t.paren,
+        '{': t.brace,
+        '}': t.brace,
+        '=>': t.keyword,
         ElementIdentifier: t.tagName,
-        "ElementStartTag ElementStartCloseTag ElementSelfCloseEndTag ElementEndTag":
+        'ElementStartTag ElementStartCloseTag ElementSelfCloseEndTag ElementEndTag':
           t.angleBracket,
-        "ElementAttribute/VariableName": t.attributeName,
+        'ElementAttribute/VariableName': t.attributeName,
       }),
     ],
   }),
   languageData: {
-    closeBrackets: { brackets: ["(", "[", "{", "'", '"', "`"] },
-    commentTokens: { line: "//", block: { open: "/*", close: "*/" } },
+    closeBrackets: { brackets: ['(', '[', '{', "'", '"', '`'] },
+    commentTokens: { line: '//', block: { open: '/*', close: '*/' } },
     indentOnInput: /^\s*(?:case |default:|\{|\}|<\/)$/,
-    wordChars: "$",
+    wordChars: '$',
   },
 });
 
