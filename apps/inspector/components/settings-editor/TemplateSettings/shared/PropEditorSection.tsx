79a5a7bf9c96beb5ceca826c917a1347d08ea1ed
Prev Wong <prevwong@gmail.com>
2022-12-15 17:15:45 +0700

    feat: add ui for classlist

--- apps/inspector/components/settings-editor/TemplateSettings/shared/PropEditorSection.tsx
+++ apps/inspector/components/settings-editor/TemplateSettings/shared/PropEditorSection.tsx
@@ -7,7 +7,6 @@ import { SettingSection } from '../../SettingSection';
 
 import { PairInput } from '@app/components/pair-input';
 import { useEditor } from '@app/editor';
-import { Text } from '@app/components/text';
 
 const stringifier = new Stringifier();
 const parser = new Parser();
@@ -19,43 +18,81 @@ type PropEditorSectionProps = {
 export const PropEditorSection = observer(
   ({ template }: PropEditorSectionProps) => {
     const [addNewProp, setAddNewProp] = React.useState(false);
+    const [addNewClassListItem, setAddNewClassListItem] = React.useState(false);
     const editor = useEditor();
 
+    const classList = template.classList;
+
     return (
-      <SettingSection
-        onAdd={() => setAddNewProp(true)}
-        title={'Props'}
-        collapsedOnInitial={false}
-      >
-        {!addNewProp && Object.keys(template.props).length === 0 && (
-          <Text size="1">There're no props on this template.</Text>
-        )}
-        <PairInput
-          addingNewField={addNewProp}
-          onCancelAdding={() => setAddNewProp(false)}
-          onChange={(id, value) => {
-            try {
-              const parsedValue = parser.parseExpressionFromSource(value);
-              //   console.log("parsedValue", parsedValue);
-              editor.state.change(() => {
-                template.props[id] = parsedValue;
-              });
-            } catch (err) {
-              // TODO: handle error
-              console.warn(err);
+      <React.Fragment>
+        <SettingSection
+          title={'Class List'}
+          onAdd={() => setAddNewClassListItem(true)}
+        >
+          <PairInput
+            addingNewField={addNewClassListItem}
+            onCancelAdding={() => setAddNewClassListItem(false)}
+            values={
+              classList
+                ? Object.keys(classList.properties).map((key) => ({
+                    id: key,
+                    value: stringifier.toString(classList.properties[key]),
+                  }))
+                : []
             }
-          }}
-          onRemove={(id) => {
-            editor.state.change(() => {
-              delete template.props[id];
-            });
-          }}
-          values={Object.keys(template.props).map((prop) => ({
-            id: prop,
-            value: stringifier.toString(template.props[prop]),
-          }))}
-        />
-      </SettingSection>
+            onChange={(id, value) => {
+              try {
+                const parsedValue = parser.parseExpressionFromSource(value);
+                //   console.log("parsedValue", parsedValue);
+                editor.state.change(() => {
+                  if (!template.classList) {
+                    template.classList = t.objectExpression({
+                      properties: {},
+                    });
+                  }
+
+                  template.classList.properties[id] = parsedValue;
+                });
+              } catch (err) {
+                // TODO: handle error
+                console.warn(err);
+              }
+            }}
+          ></PairInput>
+        </SettingSection>
+        <SettingSection
+          onAdd={() => setAddNewProp(true)}
+          title={'Props'}
+          collapsedOnInitial={false}
+        >
+          <PairInput
+            addingNewField={addNewProp}
+            onCancelAdding={() => setAddNewProp(false)}
+            emptyValuesText={'No props set for this template'}
+            onChange={(id, value) => {
+              try {
+                const parsedValue = parser.parseExpressionFromSource(value);
+                //   console.log("parsedValue", parsedValue);
+                editor.state.change(() => {
+                  template.props[id] = parsedValue;
+                });
+              } catch (err) {
+                // TODO: handle error
+                console.warn(err);
+              }
+            }}
+            onRemove={(id) => {
+              editor.state.change(() => {
+                delete template.props[id];
+              });
+            }}
+            values={Object.keys(template.props).map((prop) => ({
+              id: prop,
+              value: stringifier.toString(template.props[prop]),
+            }))}
+          />
+        </SettingSection>
+      </React.Fragment>
     );
   }
 );
