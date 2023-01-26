5cf1b86be4bb4c0a3280b3197ec5faf977bc22d3
Prev Wong <prevwong@gmail.com>
2023-01-26 16:04:50 +0700

    chore: add label in UserAnimation

--- site/external/UserAnimation.tsx
+++ site/external/UserAnimation.tsx
@@ -25,6 +25,9 @@ export const UserAnimation = () => {
           }}
         />
       </div>
+      <div className="px-2 py-2 w-full bg-indigo-700 text-white/60 text-xs text-center">
+        This is a React Component + Framer motion
+      </div>
     </div>
   );
 };
