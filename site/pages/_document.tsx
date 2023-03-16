9894a2e9e7e5fa4e2f9a39f8944e727d7f17d434
Prev Wong <prevwong@gmail.com>
2023-03-16 23:02:45 +0700

    chore: cleanup

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -1,17 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import * as React from 'react';
 
-import { getCssText } from '@app/styles';
-
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <style
-          id="stitches"
-          dangerouslySetInnerHTML={{ __html: getCssText() }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
