b49eab94511cf3f4ecd399c0a41f3a69a4c17691
Prev Wong <prevwong@gmail.com>
2023-03-26 18:42:47 +0800

    fix: use next/link in algolia search

--- site/components/toolbar-doc/index.tsx
+++ site/components/toolbar-doc/index.tsx
@@ -1,10 +1,22 @@
+import {
+  InternalDocSearchHit,
+  StoredDocSearchHit,
+} from '@docsearch/react/dist/esm/types';
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import * as React from 'react';
 
 const DocSearch = dynamic(() =>
   import('@docsearch/react').then((mod) => mod.DocSearch)
 );
+
+const HitComponent = (props: {
+  hit: InternalDocSearchHit | StoredDocSearchHit;
+  children: React.ReactNode;
+}) => {
+  return <Link href={props.hit.url}>{props.children}</Link>;
+};
 
 export const ToolbarDoc = () => {
   const router = useRouter();
@@ -15,6 +27,7 @@ export const ToolbarDoc = () => {
         appId="ELH5DZYDRA"
         apiKey="b76ae5f30294a66c17ff98f4a47713ff"
         indexName="reka-js"
+        hitComponent={HitComponent}
         navigator={{
           navigate: (params) => {
             router.push(params.itemUrl);
