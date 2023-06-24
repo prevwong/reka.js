205f077ca8fe266292903d232775737020eab49c
Prev Wong <prevwong@gmail.com>
2023-06-24 16:38:52 +0800

    fix: variable references in initial state values

--- packages/core/src/expression.ts
+++ packages/core/src/expression.ts
@@ -1,18 +1,27 @@
 import * as t from '@rekajs/types';
 import { invariant } from '@rekajs/utils';
-import { action } from 'mobx';
+import { action, untracked } from 'mobx';
 
 import { Environment } from './environment';
 import { Reka } from './reka';
 
+type ExprContext = {
+  untrackIdentifier: boolean;
+};
+
+const DEFAULT_EXPR_CONTEXT: ExprContext = {
+  untrackIdentifier: false,
+};
+
 export const computeExpression = (
   expr: t.Any,
   reka: Reka,
-  env: Environment
+  env: Environment,
+  ctx: ExprContext = DEFAULT_EXPR_CONTEXT
 ) => {
   if (expr instanceof t.BinaryExpression) {
-    const left = computeExpression(expr.left, reka, env);
-    const right = computeExpression(expr.right, reka, env);
+    const left = computeExpression(expr.left, reka, env, ctx);
+    const right = computeExpression(expr.right, reka, env, ctx);
 
     switch (expr.operator) {
       case '+': {
@@ -46,21 +55,21 @@ export const computeExpression = (
   }
 
   if (expr instanceof t.ArrayExpression) {
-    return expr.elements.map((el) => computeExpression(el, reka, env));
+    return expr.elements.map((el) => computeExpression(el, reka, env, ctx));
   }
 
   if (expr instanceof t.ObjectExpression) {
     return Object.keys(expr.properties).reduce(
       (accum, key) => ({
         ...accum,
-        [key]: computeExpression(expr.properties[key], reka, env),
+        [key]: computeExpression(expr.properties[key], reka, env, ctx),
       }),
       {}
     );
   }
 
   if (expr instanceof t.MemberExpression) {
-    const obj = computeExpression(expr.object, reka, env);
+    const obj = computeExpression(expr.object, reka, env, ctx);
     return obj[expr.property.name];
   }
 
@@ -69,14 +78,25 @@ export const computeExpression = (
   }
 
   if (expr instanceof t.Val) {
+    const value = computeExpression(expr.init, reka, env, {
+      ...ctx,
+      untrackIdentifier: true,
+    });
+
     env.set(expr.name, {
-      value: computeExpression(expr.init, reka, env),
+      value,
       readonly: false,
     });
     return;
   }
 
   if (expr instanceof t.Identifier) {
+    if (ctx.untrackIdentifier) {
+      return untracked(() => {
+        return env.getByIdentifier(expr);
+      });
+    }
+
     return env.getByIdentifier(expr);
   }
 
@@ -148,20 +168,20 @@ export const computeExpression = (
   }
 
   if (expr instanceof t.IfStatement) {
-    const bool = computeExpression(expr.condition, reka, env);
+    const bool = computeExpression(expr.condition, reka, env, ctx);
 
     if (bool) {
-      computeExpression(expr.consequent, reka, env);
+      computeExpression(expr.consequent, reka, env, ctx);
     }
   }
 
   if (expr instanceof t.ConditionalExpression) {
-    const bool = computeExpression(expr.condition, reka, env);
+    const bool = computeExpression(expr.condition, reka, env, ctx);
 
     if (bool) {
-      return computeExpression(expr.consequent, reka, env);
+      return computeExpression(expr.consequent, reka, env, ctx);
     }
 
-    return computeExpression(expr.alternate, reka, env);
+    return computeExpression(expr.alternate, reka, env, ctx);
   }
 };
