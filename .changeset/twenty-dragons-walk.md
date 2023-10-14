6e9a337ef9bd95e25ccfae9fcb1756e512cdda9f
Prev Wong <prevwong@gmail.com>
2023-10-14 20:02:18 +0200

    feat: support logical expressions (#92)

---
+++ .changeset/twenty-dragons-walk.md
@@ -0,0 +1,7 @@
+---
+'@rekajs/parser': patch
+'@rekajs/types': patch
+'@rekajs/core': patch
+---
+
+Support logical expressions
--- packages/types/src/types.definition.ts
+++ packages/types/src/types.definition.ts
@@ -108,6 +108,7 @@ Schema.define('BinaryExpression', {
       '>=',
       '||',
       '&&',
+      '??',
       '^',
       '%'
     ),
